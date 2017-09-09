1d845d20164b94c52165401cc0af43fe28d3f7e4
GooZy <ziyaoguo@gmail.com>
2017-09-09 19:54:19 +0800

    update

--- upload.sh
+++ upload.sh
@@ -1,4 +1,7 @@
-hexo clean && hexo g && hexo deploy && hexo clean
+./node_modules/.bin/hexo clean
+./node_modules/.bin/hexo generate
+./node_modules/.bin/hexo deploy
+./node_modules/.bin/hexo clean
 git add -A
 git commit -m 'update'
 git push
